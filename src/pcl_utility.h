dc745a0cf219d7a2a658258bcd268d0e6fb7e994
beizhengren <291087211@qq.com>
2023-02-17 16:41:19 +0800

    feature: add combine name

--- src/pcl_utility.h
+++ src/pcl_utility.h
@@ -19,7 +19,33 @@ void write_pcl(const std::string& name, const PCL_TYPE& cloud)
 }
 
 template <typename... PCL_POINTER>
-auto concatenate_points(const PCL_POINTER... cloud_pack)
+auto concatenate_points(const PCL_POINTER& ... cloud_pack)
 {
     return (... + *cloud_pack );
+}
+
+auto remove_suffix(const std::string& name)
+{
+  auto pos = name.find(".");
+  auto new_name = name.substr(0, pos);
+  return new_name;
+}
+
+template <typename ... PACK>
+auto concatenate_name(const PACK& ... names)
+{
+  return (... + names);
+}
+
+template <typename ... Args>
+auto combine_name(Args&& ... args)
+{
+  auto names = std::vector<std::string>{std::forward<Args>(args)...};
+  std::transform(begin(names), end(names), begin(names), remove_suffix);
+  auto ret_name = std::string{};
+  for (const auto& name : names)
+  {
+    ret_name += name;
+  }
+  return ret_name;
 }
--- unit_test/PCL_utility_test.cpp
+++ unit_test/PCL_utility_test.cpp
@@ -1,6 +1,7 @@
 #include "src/pcl_utility.h"
 
-#include <memory>
+#include <vector>
+#include <algorithm>
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv)
@@ -9,7 +10,23 @@ int main(int argc, char **argv)
   return RUN_ALL_TESTS();
 }
 
-TEST(MergePCLTestCase, PrepartionsCompleted)
+TEST(MergePCLTestCase, merge_2_PCL)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_a (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_b (new pcl::PointCloud<pcl::PointXYZ>);
+  
+  auto name_a = std::string{"test_a.pcd"};
+  auto name_b = std::string{"test_b.pcd"};
+
+  ASSERT_TRUE(pcl::io::loadPCDFile<pcl::PointXYZ> (name_a, *cloud_a) != -1);
+  ASSERT_TRUE(pcl::io::loadPCDFile<pcl::PointXYZ> (name_b, *cloud_b) != -1);
+  
+  auto cloud_data = concatenate_points(cloud_a, cloud_b);
+  auto output_name = concatenate_name(remove_suffix(name_a) + remove_suffix(name_b)) + ".pcd";
+  write_pcl(output_name, cloud_data);
+}
+
+TEST(MergePCLTestCase, merge_3_PCL)
 {
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_a (new pcl::PointCloud<pcl::PointXYZ>);
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_b (new pcl::PointCloud<pcl::PointXYZ>);
@@ -24,6 +41,18 @@ TEST(MergePCLTestCase, PrepartionsCompleted)
   ASSERT_TRUE(pcl::io::loadPCDFile<pcl::PointXYZ> (name_c, *cloud_c) != -1);
   
   auto cloud_data = concatenate_points(cloud_a, cloud_b, cloud_c);
-  auto output_name = std::string{"merged3.pcd"};
+  auto output_name = combine_name(name_a, name_b, name_c) + ".pcd";
   write_pcl(output_name, cloud_data);
+
+}
+
+TEST(CombineNameTestCase, combine_name)
+{
+  auto name_a = "test_a.pcd";
+  auto name_b = "test_b.pcd";
+  auto name_c = "test_c.pcd";
+  auto name1 = concatenate_name(remove_suffix(name_a) + remove_suffix(name_b) + remove_suffix(name_c)) + ".pcd";
+  auto name2 = combine_name(name_a, name_b, name_c) + ".pcd";
+  EXPECT_EQ(name1, name2);
+
 }
